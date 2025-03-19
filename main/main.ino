ea70936479506a74683688b59e42038be32c6cd7
PeterBlacken <ppgaldames@gmail.com>
2025-03-19 13:05:17 -0300

    separete led functions in led.h file

--- main/main.ino
+++ main/main.ino
@@ -1,6 +1,7 @@
 #include <CodeCell.h>
 #include"BLE_config.h"
 #include <BNO085.h>
+#include <led.h>
 CodeCell myCodeCell;
 
 float Roll = 0.0;
@@ -100,39 +101,6 @@ void loop()
 
 }
 
-void led_init()
-{
-  pinMode(LED_PIN, OUTPUT);   /*Set LED pin as output*/
-  digitalWrite(LED_PIN, LOW); /*Init Set up to output low*/
-  delay(1);
-  rmtInit(LED_PIN, RMT_TX_MODE, RMT_MEM_NUM_BLOCKS_1, 10000000); /*Configure RMT to run the onboard addressable LED*/
-
-  LED(0, 0, 0);
-  delay(1);
-  LED(0, 0, LED_SLEEP_BRIGHTNESS);
-  delay(80);
-  LED(LED_SLEEP_BRIGHTNESS, 0, 0);
-  delay(80);
-  LED(0, LED_SLEEP_BRIGHTNESS, 0);
-  delay(80);
-  LED(0, 0, 0);
-  delay(80);
-}
-
-void rainbow()
-{
-  LED(0, 0, 0);
-  delay(1);
-  LED(0, 0, 50u);
-  delay(80);
-  LED(50u, 0, 0);
-  delay(80);
-  LED(0, 50u, 0);
-  delay(80);
-  LED(0, 0, 0);
-  delay(80);
-}
-void LED(uint8_t r, uint8_t g, uint8_t b) {neopixelWrite(LED_PIN, r, g, b); /*RMT ESP32 function for addressable LEDs*/}
 void update_acc()
 {
 
